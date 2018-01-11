77072599939ac152005bef2d6aefce4a3f91bc49
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-01-11 10:16:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 const mustacheExpress = require('mustache-express')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
-const promise = require('bluebird')
 const express = require('express')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/public', express.static('public'))
-app.use(express.static('script'))
 app.engine("mustache", mustacheExpress())
 app.use(methodOverride("_method"))
+app.use(express.static('script'))
 app.set("view engine","mustache")
 app.set('views', './views')
-
-var options = {
-	promiseLib : promise
-}
-
-// CONFIGURING POSTGRESQL
-var connectionString = 'postgres://localhost:5432/jayzuss'
-const pgp = require('pg-promise')(options)
-var db = pgp(connectionString)
-
+db = require('./queries')
 
 // RESTfull ROUTING
 app.get('/', (req,res) => {
@@ -29,41 +19,23 @@ app.get('/', (req,res) => {
 })
 
 // STORES PAGE, DISPLAY ALL STORES
-app.get('/stores', (req,res) => {
-	db.any('SELECT * FROM stores').then( (storesdata) => {
-		res.render('stores', {stores : storesdata})
-	})
-})
+app.get('/stores', db.redirectToStores)
 
 // NAVIGATING TO CREATE A STORE PAGE
-app.get('/stores/newstore', (req,res) => {
-		res.render('newstore')
-})
+app.get('/stores/newstore', db.newStore)
 
 // CREATE A NEW STORE
-app.post('/stores', (req,res) => {
-	let storename = req.body.storename
-	let addressone = req.body.addressone
-	let addresstwo = req.body.addresstwo
-	let city = req.body.city
-	let state = req.body.state
-	let zip = parseInt(req.body.zip)
+app.post('/stores', db.saveStore)
 
-	db.none('INSERT INTO stores(name,addressone,addresstwo,city,state,zip)' + 
-	'values(${storename},${addressone},${addresstwo},${city},${state},${zip})',req.body)
+// SHOW ROUTE
+app.get('/stores/:id', db.showStore)
 
-	db.any('SELECT * from stores').then( (storesdata) => {
-		res.redirect('/stores')
-	})
+// DELETE ROUTE
+app.delete('/stores/:id', (req,res) => {
+	var storeid = parseInt(req.params.id)
+	db.result('DELETE FROM stores WHERE storeid = $1', storeid)
 })
 
-// GET STORES ID
-app.get('/stores/:id', (req,res) => {
-	db.any("SELECT * FROM stores WHERE storeid = " + "'" + req.params.id +"'")
-	.then( (data) => {
-		res.send(data)
-	})
-})
 
 app.listen(3000, () => {
 	console.log("We are live on channel 3000")
